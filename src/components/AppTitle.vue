327f78ee78fcc3a2098819ed7702c6b5bf0fb09e
ryampeguero <ryampeguero@gmail.com>
2024-04-30 14:02:39 +0200

    feat: project scaffolding

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default {
+  data(){
+    return{
+      titleText: "La mia prima app con Vite!",
+    };
+  },
+
+  created(){
+    console.log("ciao");
+  }
+}
+</script>
+
+<template>
+  <h1>{{ titleText }}</h1>
+</template>
